072f07cdc7745a249557a67518b4f6e403aad177
Jerusha Johnson <75500505+jeru-jo@users.noreply.github.com>
2021-01-23 02:18:02 -0800

    update to eslint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable no-unused-vars */
+import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/SmallProjects.jsx
+++ src/SmallProjects.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Row, Col, CardDeck, Card } from 'react-bootstrap';
 
 const ProjectList = [
